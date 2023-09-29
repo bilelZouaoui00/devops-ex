978bbc3c6353118eb6b47711eaaa19844a73ee19
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-09-29 10:51:07 +0100

    fea: Include additional comments as part of the implementation

--- src/main/java/tn/esprit/demo/entity/User.java
+++ src/main/java/tn/esprit/demo/entity/User.java
@@ -6,12 +6,18 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
+//indicate that this class is an entity
 @Entity
+//Generate setter and getter automatically
 @Setter
 @Getter
 
+//Serializable --> this class can be serializable and deserializable to enhance performance of
+    // Sorting objects, Object transmission, Caching and Session Manager.
 public class User implements Serializable {
+    //specify the Primary Key
     @Id
+    //identify the primary key and specify the strategy of PK that is auto-increment
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
     private String cin;
