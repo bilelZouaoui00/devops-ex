17993883086534b2338d58e291748a9b8f0a0e54
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-11-27 23:40:27 +0100

    Add CRUD operations to ChambreController:
    - Added endpoint for adding a Chambre using POST
    - Added endpoint for updating a Chambre using PUT
    - Added endpoint for deleting a Chambre using DELETE
    - Added endpoint for retrieving a Chambre by ID using GET
    - Added endpoint for retrieving all Chambres using GET

--- src/main/java/tn/esprit/demo/Controller/ChambreController.java
+++ src/main/java/tn/esprit/demo/Controller/ChambreController.java
@@ -1,4 +1,49 @@
 package tn.esprit.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.demo.entity.Chambre;
+import tn.esprit.demo.services.ChambreServiceImpl;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chambre")
 public class ChambreController {
+
+    private final ChambreServiceImpl chambreService;
+
+    @Autowired
+    public ChambreController(ChambreServiceImpl chambreService) {
+        this.chambreService = chambreService;
+    }
+
+    @PostMapping("/ajouter")
+    public void ajouterChambre(@RequestBody Chambre chambre) {
+        chambreService.ajouterChambre(chambre);
+    }
+
+    @PutMapping("/modifier")
+    public void modifierChambre(@RequestBody Chambre chambre) {
+        chambreService.updateChambre(chambre);
+    }
+
+    @DeleteMapping("/supprimer/{id}")
+    public void supprimerChambre(@PathVariable long id) {
+        chambreService.supprimerChambre(id);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Chambre> getChambreById(@PathVariable long id) {
+        Chambre chambre = chambreService.getChambre(id);
+        return ResponseEntity.ok(chambre);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Chambre>> getAllChambres() {
+        List<Chambre> chambres = chambreService.getAllChambre();
+        return ResponseEntity.ok(chambres);
+    }
 }
+
