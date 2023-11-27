1ca43cb1d617629e0dc4f05310d0e71bcf9d2035
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-11-27 16:08:11 +0100

    Add CRUD operations to FoyerController:
    - Added endpoint for adding a Foyer using POST
    - Added endpoint for updating a Foyer using PUT
    - Added endpoint for deleting a Foyer using DELETE
    - Added endpoint for retrieving a Foyer by ID using GET
    - Added endpoint for retrieving all Foyers using GET

--- src/main/java/tn/esprit/demo/Controller/FoyerController.java
+++ src/main/java/tn/esprit/demo/Controller/FoyerController.java
@@ -1,4 +1,49 @@
 package tn.esprit.demo.Controller;
 
-public class FoyerController {
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.demo.entity.Foyer;
+import tn.esprit.demo.services.FoyerServicesImpl;
+
+import java.util.List;
+
+@RestController
+    @RequestMapping("/foyer")
+    public class FoyerController {
+
+        private final FoyerServicesImpl foyerService;
+
+        @Autowired
+        public FoyerController(FoyerServicesImpl foyerService) {
+            this.foyerService = foyerService;
+        }
+
+        @PostMapping("/ajouter")
+        public void ajouterFoyer(@RequestBody Foyer foyer) {
+            foyerService.ajouterFoyer(foyer);
+        }
+
+        @PutMapping("/modifier")
+        public void modifierFoyer(@RequestBody Foyer foyer) {
+            foyerService.updateFoyer(foyer);
+        }
+
+        @DeleteMapping("/supprimer/{id}")
+        public void supprimerFoyer(@PathVariable long id) {
+            foyerService.supprimerFoyer(id);
+        }
+
+        @GetMapping("/get/{id}")
+        public ResponseEntity<Foyer> getFoyerById(@PathVariable long id) {
+            Foyer foyer = foyerService.getFoyer(id);
+            return ResponseEntity.ok(foyer);
+        }
+
+        @GetMapping("/getAll")
+        public ResponseEntity<List<Foyer>> getAllFoyers() {
+            List<Foyer> foyers = foyerService.getAllFoyer();
+            return ResponseEntity.ok(foyers);
+        }
+    }
+
