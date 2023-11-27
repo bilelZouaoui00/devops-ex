1c5be23ea729cb5de91db1781ec44da95ca8800a
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2023-11-28 00:00:29 +0100

    Add CRUD operations to ReservationController:
    - Added endpoint for adding a Reservation using POST
    - Added endpoint for updating a Reservation using PUT
    - Added endpoint for deleting a Reservation using DELETE
    - Added endpoint for retrieving a Reservation by ID using GET
    - Added endpoint for retrieving all Reservations using GET

--- src/main/java/tn/esprit/demo/Controller/ReservationController.java
+++ src/main/java/tn/esprit/demo/Controller/ReservationController.java
@@ -1,4 +1,49 @@
 package tn.esprit.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.demo.entity.Reservation;
+import tn.esprit.demo.services.ReservationServiceImpl;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservation")
 public class ReservationController {
+
+    private final ReservationServiceImpl reservationService;
+
+    @Autowired
+    public ReservationController(ReservationServiceImpl reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping("/ajouter")
+    public void ajouterReservation(@RequestBody Reservation reservation) {
+        reservationService.ajouterReservation(reservation);
+    }
+
+    @PutMapping("/modifier")
+    public void modifierReservation(@RequestBody Reservation reservation) {
+        reservationService.updateReservation(reservation);
+    }
+
+    @DeleteMapping("/supprimer/{id}")
+    public void supprimerReservation(@PathVariable long id) {
+        reservationService.supprimerReservation(id);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable long id) {
+        Reservation reservation = reservationService.getReservation(id);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        List<Reservation> reservations = reservationService.getAllReservations();
+        return ResponseEntity.ok(reservations);
+    }
 }
+
