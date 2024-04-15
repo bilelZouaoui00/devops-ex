2bbb9d5b864b77872f62e1e226fe280e4da98646
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2024-04-15 02:47:19 +0100

    fix : junit test

--- src/test/java/tn/esprit/demo/DemoApplicationTests.java
+++ src/test/java/tn/esprit/demo/DemoApplicationTests.java
@@ -2,11 +2,8 @@ package tn.esprit.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 
 @SpringBootTest
-
 class DemoApplicationTests {
 
     @Test
