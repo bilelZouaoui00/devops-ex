8515afd2144326c778b6be4e859448e1cf232c22
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2024-04-15 02:31:08 +0100

    added junit test

--- src/test/java/tn/esprit/demo/DemoApplicationTests.java
+++ src/test/java/tn/esprit/demo/DemoApplicationTests.java
@@ -2,8 +2,11 @@ package tn.esprit.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
+
 class DemoApplicationTests {
 
     @Test
